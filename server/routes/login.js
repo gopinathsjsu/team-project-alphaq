e9812a1c9e83afc5ebe11968015c63e982591451
mohana <mohana4984@gmail.com>
2023-10-29 17:22:28 -0700

    updated all the login

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 router.post('/login', async (req, res) => {
-    try {
+   
         const { email, password } = req.body;
         const user = await User.findOne({ email });
         if (user && await bcrypt.compare(password, user.password)) {
@@ -10,9 +10,7 @@ router.post('/login', async (req, res) => {
         } else {
             res.status(401).json({ message: 'Invalid credentials' });
         }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+    
 });
 
 module.exports = router;
