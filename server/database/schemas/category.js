75b082c8ee149f48b82f4d2b4e3cb24ba4de1f94
Dead-Stone <mohana4984@gmail.com>
2023-11-20 17:26:30 -0800

    Updated the schema

---
+++ server/database/schemas/category.js
@@ -0,0 +1,36 @@
+// const { AutoIncrementID } = require('@typegoose/auto-increment');
+// const bcrypt = require('bcryptjs');
+// const mongoose = require('mongoose');
+// const R = require('ramda');
+
+// const { Schema } = mongoose;
+
+// var categorySchema = new Schema({
+//     category_name: {
+//         type: String,
+//         required: true
+//     },
+//     tags: {
+//         type: Array,
+//         required:true
+//     },
+//     description: {
+//         type: String,
+//         required:true
+//     },
+//     is_active: {
+//         type: Boolean,
+//         default:true
+//     }
+// });
+
+// var interest_exports = mongoose.model('category_details', interestSchema);
+
+// // var category = new interest_exports({
+// //     category_name: "Cricket",
+// //     tags: ["Bats", "Bolls", "Sports_Game"],
+// //     description:"Cricket is a Sport Game"
+// // });
+// // category.save();
+
+// module.exports = interest_exports;
