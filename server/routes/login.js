b73c9c91831488ce1c8ee4bfbab835686f3436e0
Dead-Stone <mohana4984@gmail.com>
2023-11-20 17:26:30 -0800

    Updated the schema

---
+++ server/routes/login.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (user && await bcrypt.compare(password, user.password)) {
+            // Implement JWT token generation or session management here
+            res.status(200).json({ message: 'Login successful' });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
---
+++ server/routes/signup.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('./models/User'); // import User model
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
