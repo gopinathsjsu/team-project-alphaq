4184a4d96cc81f1f9241f30a4844838d358cf77c
Christopher Vi <christopher.vi@sjsu.edu>
2023-11-26 21:27:01 -0800

    added angular material side nav

--- movie-theater-club-ui/src/app/app.component.ts
+++ movie-theater-club-ui/src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MatListModule } from '@angular/material/list';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MatSidenavModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  @ViewChild('drawer', { static: true }) drawer!: MatDrawer;
+  
   title = 'movie-theater-club-ui';
+
+  toggleMenu(value: string) {
+    this.drawer.toggle();
+  }
 }
