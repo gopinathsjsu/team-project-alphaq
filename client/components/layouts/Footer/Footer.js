ed83e4bf184fbb6d3edc602becc0ae5d9c18a5c9
mohana <mohana4984@gmail.com>
2023-11-29 22:06:20 -0800

    updated client code

--- client/components/layouts/Footer/Footer.js
+++ client/components/layouts/Footer/Footer.js
@@ -1,118 +1,23 @@
-/* eslint-disable react/jsx-curly-newline */
-/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 
-// import crossifyImg from 'assets/logos/logo_final.png';
+import Container from 'react-bulma-companion/lib/Container';
+import Content from 'react-bulma-companion/lib/Content';
+import Footer from 'react-bulma-companion/lib/Footer';
 
-export default function Footer() {
+import styles from './styles.module.css';
+
+export default function FooterComponent() {
+  const year = new Date().getFullYear();
+
   return (
-    <footer className=" bg-gray-300 pt-8 pb-6">
-      <div
-        className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
-        style={{ transform: 'translateZ(0)' }}
-      >
-        <svg
-          className="absolute bottom-0 overflow-hidden"
-          xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="none"
-          version="1.1"
-          viewBox="0 0 2560 100"
-          x="0"
-          y="0"
-        >
-          <polygon
-            className="text-gray-300 fill-current"
-            points="2560 0 2560 100 0 100"
-          />
-        </svg>
-      </div>
-      <div className="container mx-auto px-4">
-        <div className="flex flex-wrap text-center lg:text-left">
-          <div className="w-full lg:w-6/12 px-4 ">
-            <img
-              alt="brandphoto"
-              // TODO : Add brand icon
-              src=""
-              className="mr-2 inline-block"
-              style={{ height: 30 }}
-            />
-            <h4 className="text-3xl font-semibold inline-block pb-2">
-              Crossify
-            </h4>
-            <h5 className="text-lg mt-0 mb-2 text-gray-700">
-              Find your next favourite movie
-            </h5>
-          </div>
-          <div className="w-full lg:w-6/12 px-4">
-            <div className="flex flex-wrap items-top mb-6">
-              <div className="w-full lg:w-6/12 px-4 ml-auto">
-                <div className="mt-6 lg:mb-0 mb-6">
-                  <button
-                    className="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center
-                     align-center rounded-full outline-none focus:outline-none mr-2"
-                    type="button"
-                    onClick={() => {
-                      window.open(
-                        ' https://twitter.com/moviebookbyalphaq',
-                        '_blank',
-                      );
-                    }}
-                  >
-                    <i className="fab fa-twitter" />
-                  </button>
-                  <button
-                    className="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center
-                    align-center rounded-full outline-none focus:outline-none mr-2"
-                    type="button"
-                    onClick={() =>
-                      window.open(
-                        'https://www.facebook.com/groups/510718810086566',
-                        '_blank',
-                      )
-                    }
-                  >
-                    <i className="fab fa-facebook-square" />
-                  </button>
-                  <button
-                    className="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center
-                    align-center rounded-full outline-none focus:outline-none mr-2"
-                    type="button"
-                    onClick={() => {
-                      window.open(
-                        'https://www.linkedin.com/groups/9049763/',
-                        '_blank',
-                      );
-                    }}
-                  >
-                    <i className="fab fa-linkedin" />
-                  </button>
-                  <button
-                    className="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center
-                     align-center rounded-full outline-none focus:outline-none mr-2"
-                    type="button"
-                    onClick={() => {
-                      window.open(
-                        'https://www.github.com/dhruvilshah191999/Crossify',
-                        '_blank',
-                      );
-                    }}
-                  >
-                    <i className="fab fa-github" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <hr className="my-6 border-gray-400" />
-        <div className="flex flex-wrap items-center md:justify-between justify-center">
-          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-            <div className="text-sm text-gray-600 font-semibold py-1 ">
-              MovieBook by AlphaQ
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <Footer className={styles.root}>
+      <Container>
+        <Content className={styles.content} textAlign="center">
+          <p>
+            {`Copyright Ⓒ ${year} MERN Boilerplate. All Rights Reserved.`}
+          </p>
+        </Content>
+      </Container>
+    </Footer>
   );
 }
