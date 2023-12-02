c12ceab68070a1d16602322e9fff0872aff0a440
Christopher Vi <christopher.vi@sjsu.edu>
2023-12-01 18:55:05 -0800

    updated create account link on home page

--- movie-theater-club-ui/src/app/home/home.component.ts
+++ movie-theater-club-ui/src/app/home/home.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../login/login.component';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, LoginComponent],
+  imports: [CommonModule, LoginComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) { }
 
+  routeToCreateAccount() {
+    this.router.navigateByUrl('/create-account')
+  }
 }
