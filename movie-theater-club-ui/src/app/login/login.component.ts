676605ad6384bf7940d1e5008616f697d0f5ee54
Christopher Vi <christopher.vi@sjsu.edu>
2023-12-01 21:47:25 -0800

    updated login component

--- movie-theater-club-ui/src/app/login/login.component.ts
+++ movie-theater-club-ui/src/app/login/login.component.ts
@@ -2,18 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AngularMaterialModule } from '../angular-material.module';
+import { Router, RouterModule } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, AngularMaterialModule, FormsModule],
+  imports: [CommonModule, AngularMaterialModule, FormsModule, RouterModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   public loginValid = true;
-  public username = "";
+  public email = "";
   public password = "";
 
-  public onSubmit(): void {}
+  constructor(private router: Router, private httpClient: HttpClient) { }
+
+  public onSubmit(): void {
+    const body = {"email":this.email,"password":this.password};
+    let http = this.httpClient.post("http://localhost:8080/account/login", body).subscribe(response => {
+      console.log(response);
+
+      let data: any = response;
+      if(data["role"] == null)
+        this.loginValid = false;
+    })
+  }
 }
