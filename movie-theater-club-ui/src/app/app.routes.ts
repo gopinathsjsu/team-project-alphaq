f243e5b051ed83792c3186f8bc874aa86f4a1b4a
Christopher Vi <christopher.vi@sjsu.edu>
2023-12-01 18:57:23 -0800

    updated routes for create account component

--- movie-theater-club-ui/src/app/app.routes.ts
+++ movie-theater-club-ui/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'create-account', component: CreateAccountComponent }
 ];
