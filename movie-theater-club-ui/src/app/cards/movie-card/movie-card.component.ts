847c761d0d71bba14a286d031ea1014e75dd1edc
Christopher Vi <christopher.vi@sjsu.edu>
2023-12-03 00:57:21 -0800

    updated movie card component for book tickets payment component

--- movie-theater-club-ui/src/app/cards/movie-card/movie-card.component.ts
+++ movie-theater-club-ui/src/app/cards/movie-card/movie-card.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatTooltipModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatTooltipModule, RouterModule],
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.css'
 })
@@ -22,6 +23,8 @@ export class MovieCardComponent implements OnInit {
   description = "";
   posterFileName = "";
 
+  constructor(private router: Router) {}
+
   ngOnInit(): void {
     this.id = this.movie["id"];
     this.title = this.movie["title"];
@@ -30,5 +33,10 @@ export class MovieCardComponent implements OnInit {
     this.showtime = this.movie["showtime"];
     this.description = this.movie["description"];
     this.posterFileName = this.movie["posterFileName"];
-  }  
+  }
+  
+  routeToPayment(selectedShowtime: string):void {
+    this.router.navigate(['/book-tickets-payment'], 
+    {queryParams: {'movieId':this.id,'movieTitle':this.title,'selectedShowtime': selectedShowtime}})
+  }
 }
