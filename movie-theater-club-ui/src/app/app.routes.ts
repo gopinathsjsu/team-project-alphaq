f3113c1bb2670024ddb1567c4991b3f036c192ed
Christopher Vi <christopher.vi@sjsu.edu>
2023-12-03 00:56:24 -0800

    updated routes for book tickets payment component

--- movie-theater-club-ui/src/app/app.routes.ts
+++ movie-theater-club-ui/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { BookTicketsComponent } from './book-tickets/book-tickets.component';
+import { BookTicketsPaymentComponent } from './book-tickets-payment/book-tickets-payment.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', component: HomeComponent },
     { path: 'create-account', component: CreateAccountComponent },
-    { path: 'book-tickets', component: BookTicketsComponent }
+    { path: 'book-tickets', component: BookTicketsComponent },
+    { path: 'book-tickets-payment', component: BookTicketsPaymentComponent }
 ];
