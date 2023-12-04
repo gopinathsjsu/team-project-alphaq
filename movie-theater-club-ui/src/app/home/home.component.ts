25387cc9bcaa54b2bf22bb44e3ad6d56a97240f8
Christopher Vi <christopher.vi@sjsu.edu>
2023-12-03 22:16:54 -0800

    updated home page

--- movie-theater-club-ui/src/app/home/home.component.ts
+++ movie-theater-club-ui/src/app/home/home.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../login/login.component';
 import { Router, RouterModule } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, LoginComponent, RouterModule, MatGridListModule],
+  imports: [CommonModule, LoginComponent, RouterModule, HttpClientModule, MatGridListModule, MatListModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  constructor(private router: Router) { }
+export class HomeComponent implements OnInit {
+
+  public location: string | null = "San Jose";
+  movieSchedule = [];
+
+  constructor(private router: Router, private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+      this.getMovieSchedule(this.location);
+  }
 
   routeToCreateAccount() {
     this.router.navigateByUrl('/create-account')
@@ -20,5 +34,13 @@ export class HomeComponent {
 
   routeToBookTickets() {
     this.router.navigateByUrl('/book-tickets')
+  }
+
+  public getMovieSchedule(location: string | null): void {
+    let http = this.httpClient.get("http://localhost:8080/movie-schedule/get-schedule/" + this.location).subscribe(response => {
+      console.log(response);
+      let data: any = response;
+      this.movieSchedule = data;
+    })
   }
 }
