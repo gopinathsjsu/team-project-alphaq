db77818f03a8993ca7c22dc8117fd33f7a8389b1
mohana <mohana4984@gmail.com>
2023-12-03 20:27:45 -0800

    updated env file

--- server/config/environment.js
+++ server/config/environment.js
@@ -1,4 +1,4 @@
-process.env.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/MovieBook';
+process.env.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://dheerjain000:2OYfR3V5Zhzb7Rv6@cluster0.lgf2uw6.mongodb.net/cmpe-202';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.PORT = process.env.PORT || 3000;
-process.env.SESSION_SECRET = process.env.SESSION_SECRET || 'mern';
+process.env.SESSION_SECRET = process.env.SESSION_SECRET || 'cmpe-202';
